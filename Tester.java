30b4ad9ea6b659530cbaef09222b4e3a9c7905ec
mzimic20 <mzimic20@stuy.edu>
2020-10-22 16:35:05 -0400

    Final

--- Tester.java
+++ Tester.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class Tester {
 
   public static void main(String[]args) {
-    int[] a = {
-      1, 3, 5
-    };
     int[][] A = {
       { 1, 0, 12, -1 },
       { 7, -2, 2, 1 },
       { -5, -2, 2, -9 }
     };
-    System.out.println( ArrayOps.sum(a) );
-    System.out.println( ArrayOps.largest(a) );
+    int[] B = {
+      1, 3, 5
+    };
+    System.out.println( ArrayOps.sum(B) );
+    System.out.println( ArrayOps.largest(B) );
     System.out.println( Arrays.toString( ArrayOps.sumRows(A) ) );
     System.out.println( Arrays.toString( ArrayOps.largestInRows(A) ) );
     System.out.println( ArrayOps.sum(A) );
