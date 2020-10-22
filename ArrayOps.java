0b060f4ad0e8b09fce90a42c085e7a2476dbdc99
mzimic20 <mzimic20@stuy.edu>
2020-10-22 14:28:12 -0400

    4a. sum() works

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,23 +1,27 @@
 public class ArrayOps {
 
   public static int sum(int[] arr) {
-
+    int sum = 0;
+    for(int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   }
 
-  public static int largest(int[] arr) {
+//  public static int largest(int[] arr) {
 
-  }
+//  }
 
-  public static int[] sumRows(int[][] matrix) {
+//  public static int[] sumRows(int[][] matrix) {
 
-  }
+//  }
 
-  public static int[] largestInRows(int[][] matrix) {
+//  public static int[] largestInRows(int[][] matrix) {
 
-  }
+//  }
 
-  public static int sum(int[][] arr) {
-    
-  }
+//  public static int sum(int[][] arr) {
+
+//  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,14 @@
 public class Tester {
 
-  public static void main{
-
+  public static void main(String[]args) {
+    int[] a = {
+      1, 2, 3, 4, 5
+    };
+    int[] b = {
+      2, 4, 6, 8, 10
+    };
+    System.out.println( ArrayOps.sum(a) );
+    System.out.println( ArrayOps.sum(b) );
   }
 
 }
